a25d581c3ab098bd6aa90a094ea22cb1f573f370
P4ps11 <107376275+P4ps11@users.noreply.github.com>
2022-07-11 16:16:22 -0500

    Update Calculadora de peso.py

--- Calculadora de peso.py
+++ Calculadora de peso.py
@@ -1,4 +1,14 @@
-peso = input("¿Cuál es tu peso en kg?:  ")
-estatura = input("¿Cuál es tu estatura en metros?:  ")
-imc = round(float(peso)/float(estatura)**2)
-print("Tu índice de masa corporal es " + str(imc))
+# Programa para ver estado de peso
+peso = float(input("¿Cuanto pesas en kg?:  "))
+altura = float(input("¿Cuanto mides en metros?:  "))
+imc = round((peso) / (altura) ** 2)
+print("tu índice de masa corporal es de", imc)
+
+if imc < 18.5:
+    print("Tu peso es muy bajo, come más")
+elif imc < 25:
+    print("Tu peso esta normal, felicitaciones!")
+elif imc < 30:
+    print("Estas en sobrepeso, haz más ejercicio")
+else:
+    print("¡Estas obeso!, come menos y haz")
